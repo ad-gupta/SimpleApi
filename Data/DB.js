c988bf401fad49aa2ce647b39f1fe06bc58a8f18
adgupta-1 <21bec029@iiitdmj.ac.in>
2023-04-08 08:17:37 +0530

    fourth commit

--- Data/DB.js
+++ Data/DB.js
@@ -4,7 +4,7 @@ export const connectToDB = () => {
     mongoose.connect(process.env.MONGO_URI, {
         dbName: "shorts"
     })
-    .then(()=> console.log("database is connected"))
+    .then((c)=> console.log(`database is connected with ${c.connection.host}`))
     .catch((e)=> console.log(e))
     // console.log(`URI is ${process.env.MONGO_URI}`)
 }
